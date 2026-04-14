099570959a0ebe55d8aba784552139f4c5ff50cd
Emil Göransson <98598009+EmilGoransson@users.noreply.github.com>
2026-04-14 15:53:39 +0200

    Add Verify benchmark

--- benchmark/benchmark_multi_test.go
+++ benchmark/benchmark_multi_test.go
@@ -1,11 +1,16 @@
 package benchmark
 
 import (
+	"crypto"
 	"fmt"
+	"log"
 	"math"
 	"merkle-ocsp/internal/ocsp"
 	"merkle-ocsp/internal/tree"
 	"testing"
+	"time"
+
+	"github.com/cloudflare/circl/sign/mldsa/mldsa44"
 )
 
 // How many issued certificates that is added to the issue tree
@@ -67,8 +72,12 @@ func buildMultiEpochLandmarks(t testing.TB, totalIssued, totalRevoked, numEpochs
 		}
 
 		landmarks = append(landmarks, landmark)
-
+		// Freeze logic (like in updateController updateController.go)
 		sparseTree = combined.RevSMT
+		if j < numEpochs-1 {
+			combined.RevSMT = combined.RevSMT.Freeze()
+		}
+
 	}
 	return landmarks, allRevokedHashes[0]
 }
@@ -101,4 +110,49 @@ func BenchmarkProofSizeEpochFrequency(b *testing.B) {
 
 		}
 	}
+}
+
+// BenchClientVerify benchmarks the verify function, it does not include the signature verification
+func BenchmarkClientVerify(b *testing.B) {
+	// Fetch Signed LM + Public key + First cert
+	// use OCSP.verify for a cert
+	_, priv, err := mldsa44.GenerateKey(nil)
+	if err != nil {
+		log.Fatalf("creating key,  %v", err)
+	}
+
+	for _, numIssued := range issuedCounts {
+		for _, numRevoked := range RevokedRatios {
+			tRevoked := int(math.Max(1, math.Round(float64(numIssued)*numRevoked)))
+			for _, numEpochs := range EpochCounts {
+				b.Run(fmt.Sprintf("issued=%d/revoked=%.0f%%/epochs=%d", numIssued, numRevoked*100, numEpochs), func(b *testing.B) {
+					date := time.Now()
+					landmarks, targetHash := buildMultiEpochLandmarks(b, numIssued, tRevoked, numEpochs)
+
+					tLandmark := landmarks[0]
+
+					newestLandmark := landmarks[len(landmarks)-1]
+
+					signedLandmark, err := newestLandmark.NewSignedHeadMLDSA(priv, crypto.SHA256, time.Second*30)
+					if err != nil {
+						log.Fatalf("creating signed landmark, %v", err)
+					}
+					resp, err := ocsp.NewResponse(targetHash, tLandmark, newestLandmark)
+
+					b.ResetTimer()
+					for i := 0; i < b.N; i++ {
+						ok, err := ocsp.Verify(resp, signedLandmark, targetHash, date)
+						if err != nil {
+							log.Fatalf("verifying response, %v", err)
+						}
+						if ok != true {
+							log.Fatalf("bad response, ok = %t", ok)
+						}
+					}
+
+				})
+			}
+		}
+	}
+
 }
